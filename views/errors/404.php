74e2e399ac10c76519be614f901abb9460f73f49
wildhanhidayatullah <wildhan.hdy@gmail.com>
2026-01-22 23:18:44 +0700

    feat: add views for errors

---
+++ views/errors/404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>404 - Not Found</title>
+    <style>
+        body {
+            text-align: center;
+            padding: 50px;
+            font-family: sans-serif;
+            color: #333;
+        }
+        h1 {
+            font-size: 50px;
+            margin-bottom: 5px;
+        }
+        p {
+            font-size: 20px;
+            color: #666;
+        }
+        a {
+            text-decoration: none;
+            color: #007bff;
+        }
+    </style>
+</head>
+<body>
+    <h1>404</h1>
+    <h2>Not Found</h2>
+    <br />
+    <a href="/">Kembali ke Dashboard</a>
+</body>
+</html>
